1b6fb64594b3c0eb429dfaf24e0a4f6d120929b0
brad <gjrjs31@naver.com>
2023-04-19 23:42:08 +0900

    feat: 프로젝트 생성 및 story보드 제거

---
+++ OurNeighborhoodLibrary/OurNeighborhoodLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OurNeighborhoodLibrary
+//
+//  Created by brad on 2023/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+    }
+
+
+}
+
