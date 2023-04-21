10d18ddbf8a42e01e7cd88ab1702d09a057662a9
brad <gjrjs31@naver.com>
2023-04-21 13:40:30 +0900

    feat: URLSessionManager 생성

--- OurNeighborhoodLibrary/OurNeighborhoodLibrary/Manager/URLSessionManager.swift
+++ OurNeighborhoodLibrary/OurNeighborhoodLibrary/Manager/URLSessionManager.swift
@@ -16,6 +16,28 @@ final class URLSessionManager {
     
     private init() { }
     
+    func getImage(urlString: String, completion: @escaping (Result<Data, Error>) -> ()) {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                if !(200...299).contains(response.statusCode) {
+                    print(response.statusCode)
+                    return
+                }
+            }
+            
+            if let data = data {
+                completion(.success(data))
+            }
+        }.resume()
+    }
+    
     func getDocsInfo(to data: GetDocsAPI, completion: @escaping (Result<Data, Error>) -> ()) {
         guard let url = api.getDocsAPI(to: data).url else {
             return
