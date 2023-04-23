d23ab2ec169b7f848121b24c289b511ed201c303
brad <gjrjs31@naver.com>
2023-04-23 23:24:10 +0900

    feat: 검색화면뷰컨트롤 생성

---
+++ OurNeighborhoodLibrary/OurNeighborhoodLibrary/Controller/SearchBooks/SearchBooksViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBooksViewController.swift
+//  OurNeighborhoodLibrary
+//
+//  Created by brad on 2023/04/23.
+//
+
+import UIKit
+
+final class SearchBooksViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemPink
+    }
+    
+}
